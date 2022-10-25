5ca36b9d37bf396dbce0afea568d3bbaa81125e6
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-25 18:45:02 -0500

    homepage functional

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 
 //styling
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
 
 //Components
 import Home from './Components/Home'
@@ -14,9 +16,38 @@ import EditArtwork from './Components/EditArtwork';
 function App() {
   return (
     <div className="App">
+      <Router>
       <header className="App-header">
-        
+        <h1 className="App-title">Art Share</h1>
       </header>
+      <nav className="App-nav">
+      <Container>
+            <Nav variant='pills' fill defaultActiveKey="/Home">
+              <Nav.Item>
+                <Link to="/">Home</Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link to="/artworks">Artworks</Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link to="/createArtwork">Create Art Post</Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
+      </nav>
+
+      <div className="display">
+        <Routes>
+        <Route path ='/' element={<Home/>} />
+        <Route path ='/artworks' element={<AllArtworks/>} />
+        <Route path ='/createArtwork' element={<CreateArtwork/>} />
+        <Route path ='/getArtwork/:id' element={<DisplayArtwork/>} />
+        <Route path ='/deleteArtwork/:id' element={<AllArtworks/>} />
+        <Route path ='/updateArtwork/:id' element={<EditArtwork/>} />
+        </Routes>
+      </div>
+
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import '../Home.css'
+
+export default function Home () {
+    return(
+        <div>
+               <h1>The Art Sharing App</h1>
+         <div className='homepage'>
+   
+         </div>
+         <div className='homepage_text'>
+          <p>
+            This is an app for users to post their art to share with the world!
+          </p>
+          
+         </div>
+        </div>
+    )
+   }
