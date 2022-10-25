dde3eaf2ae4e664c52d69a47f3f48835dc52c7cc
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-25 18:54:46 -0500

    all artworks component set up

---
+++ src/Components/AllArtworks.js
@@ -0,0 +1,55 @@
+//React features & hooks
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+
+//React-Bootstrap components
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import CardGroup from 'react-bootstrap/CardGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container'
+
+//Other Styling
+import '../Home.css'
+
+export default function AllArtworks() {
+    const [artworks, setArtworks] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+             const URL = `${process.env.REACT_APP_BACKEND_URL}/api/artworks`
+             const response = await fetch(URL)
+             const data = await response.json()
+             setArtworks(data)
+        }
+        fetchData()
+   }, [])  
+
+const display = artworks && artworks.map((eachArtwork) => {
+    return (
+        <Col xs={12} md={4} sm={6} lg={4} className='p-2' key={eachArtwork._id}>
+          <CardGroup>
+          <Card>
+               <Card.Header style={{height: '75px'}}>
+                    <Card.Title>
+                         {eachArtwork.title}
+                    </Card.Title>
+               </Card.Header>
+               <Card.Img style={{height: '300px'}} alt='Art' variant='bottom' src={eachArtwork.imgURL} />
+               <ListGroup className="list-group-flush">
+                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Artist: </span>{eachArtwork.artist}</ListGroup.Item>
+                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Size: </span>{eachArtwork.size} minutes</ListGroup.Item>
+               </ListGroup>
+               <Card.Footer>
+                    <Link to={`/getRecipe/${eachArtwork._id}`}>
+                         <Button variant="primary">Go To Artwork</Button>
+                    </Link>
+               </Card.Footer>
+          </Card>
+          </CardGroup> 
+     </Col>
+    )
+})
+}
