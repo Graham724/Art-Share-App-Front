39269cd8ecc91aef8f4241283827e99ab30b7ba2
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-25 18:31:10 -0500

    file structure update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+//React Features
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+
+//styling
 import './App.css';
+
+//Components
+import Home from './Components/Home'
+import AllArtworks from './Components/AllArtworks';
+import CreateArtwork from './Components/CreateArtwork';
+import DisplayArtwork from './Components/DisplayArtwork';
+import EditArtwork from './Components/EditArtwork';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
